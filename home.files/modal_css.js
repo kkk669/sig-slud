36e9e53202ecee4102c2280b326b97c9d49d3e28
kkk669 <kabuto669@icloud.com>
2014-05-17 21:14:59 +0900

    モーダルウィンドウのアニメーションを css で実装した場合の modal_css.css と modal_css.js を追加

---
+++ home.files/modal_css.js
@@ -0,0 +1,84 @@
+$(function() {
+  setModal();
+})
+
+function setModal() {
+
+  //HTML読み込み時にモーダルウィンドウの位置をセンターに調整
+  //adjustCenter("div#modal div.container");
+
+  //ウィンドウリサイズ時にモーダルウィンドウの位置をセンターに調整
+  /*$(window).resize(function() {
+    adjustCenter("div#modal div.container");
+  });*/
+
+  //背景がクリックされた時にモーダルウィンドウを閉じる
+  $("div#modal div.background").click(function() {
+    displayModal(false);
+  });
+
+  //リンクがクリックされた時にAjaxでコンテンツを読み込む
+  $("a.modal").click(function(event) {
+    event.preventDefault();
+    $.ajaxSetup({
+      beforeSend: function(xhr){
+        xhr.overrideMimeType("text/html; charset=iso-2022-jp");
+      }
+    });
+    $("div#modal div.container p.article").load($(this).attr("href"), data="html", onComplete);
+    return false;
+  });
+
+  //コンテンツの読み込み完了時にモーダルウィンドウを開く
+  function onComplete() {
+    displayModal(true);
+    $("div#modal div.container a.close").click(function() {
+      displayModal(false);
+      return false;
+    });
+  }
+}
+
+//モーダルウィンドウを開く
+function displayModal(sign) {
+  if (sign) {
+    // div#modal を表示
+    $("div#modal").toggle(0, function() {
+      // div.container をスクロールイン
+      $("div.container").css("-webkit-transition-duration", "0.5s");
+      $("div.container").css("transition-duration", "0.5s");
+      $("div.container").css("-webkit-transition-timing-function", "ease-out");
+      $("div.container").css("transition-timing-function", "ease-out");
+      $("div.container").addClass("scroll");
+      
+      $('div.container').animate({scrollTop: 0}, 1);
+
+      // div.background をフェードイン
+      $("div.background").fadeIn(500);
+      // 背景のスクロール禁止
+      $('body').addClass('noscroll');
+    });
+  } else {
+    // div.container をスクロールアウト
+    $("div.container").css("-webkit-transition-duration", "0.25s");
+    $("div.container").css("transition-duration", "0.25s");
+    $("div.container").css("-webkit-transition-timing-function", "ease-in");
+    $("div.container").css("transition-timing-function", "ease-in");
+    $("div.container").removeClass("scroll");
+
+    // div.background をフェードアウト
+    $("div.background").fadeOut(250, function() {
+      // div#modal 全体を非表示
+      $("div#modal").toggle();
+      // 背景のスクロール許可
+      $('body').removeClass('noscroll');
+    });
+  }
+}
+
+//ウィンドウの位置をセンターに調整
+/*function adjustCenter(target) {
+  var margin_top = ($(window).height()-$(target).height())/2;
+  var margin_left = ($(window).width()-$(target).width())/2;
+  $(target).css({top:margin_top+"px", left:margin_left+"px"});
+}*/
